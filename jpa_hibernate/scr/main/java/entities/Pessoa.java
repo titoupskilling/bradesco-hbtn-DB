86d0da51522d47771ab238863384ed58f7d44940
Renato <ren.tito@yahoo.com.br>
2025-09-05 01:32:35 +0000

    project estrutura de dados

---
+++ jpa_hibernate/scr/main/java/entities/Pessoa.java
@@ -0,0 +1,47 @@
+package main.java.entities;
+
+public class Pessoa {
+    private int id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String cpf;
+    private String dataNascimento;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
--- jpa_hibernate/src/main/java/entities/Pessoa.java
+++
@@ -1,5 +0,0 @@
-package main.java.entities;
-
-public class Pessoa {
-    
-}
