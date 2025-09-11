f5f4d9f28c70da8deb6a11d8a938cbfa4c225b6f
Renato <ren.tito@yahoo.com.br>
2025-09-11 01:03:17 +0000

    project estrutura de dados

--- java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/MaterialCurso.java
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/MaterialCurso.java
@@ -1,5 +1,22 @@
 package entities;
 
+import javax.persistence.Id;
+
 public class MaterialCurso {
+    @Id
+    private int id;
+    private String url;
     
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
--- java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Professor.java
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Professor.java
@@ -1,5 +1,38 @@
 package entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Professor {
+    @Id
+    private int id;
+    private String nomeCompleto;
+    private String matricula;
+    private String email;
     
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
